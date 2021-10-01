ce28afe002a973bbaed35cb120c066a2a3eeca38
Nguyễn Đức Long <36650293+Duclong115@users.noreply.github.com>
2021-10-02 03:02:48 +0700

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,12 @@
+Swal.fire({
+    title: 'Custom width, padding, background.',
+    width: 600,
+    padding: '3em',
+    background: '#fff url(./trees.png)',
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("./nyan-cat.gif")
+      left top
+      no-repeat
+    `
+  })
