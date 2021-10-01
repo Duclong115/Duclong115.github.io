55bbb67d7225e1bf19e950c6d366742b3002bc14
Nguyễn Đức Long <36650293+Duclong115@users.noreply.github.com>
2021-10-02 03:05:44 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,10 +2,10 @@ Swal.fire({
     title: 'Custom width, padding, background.',
     width: 600,
     padding: '3em',
-    background: '#fff url(./trees.png)',
+    background: '#fff url(trees.png)',
     backdrop: `
       rgba(0,0,123,0.4)
-      url("./nyan-cat.gif")
+      url("nyan-cat.gif")
       left top
       no-repeat
     `
